aa6cbde76aad21dd29abfaa7f6b44d84b6d08503
Gino <ginococeres2000@gmail.com>
2023-02-08 12:52:04 -0300

    Se agregaron comentarios y un nuevo console.log

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//querySelector sirve para llamar una etiqueta, clase, id del 
+//HTML, llamandolos de la forma en que se estilizaran en CSS.
+
+
 const h1 = document.querySelector("h1");
 const p = document.querySelector("p");
 const parrafito = document.getElementsByClassName(".parrafito");
@@ -14,12 +18,24 @@ console.log({
   input,
 })
 
-h1.innerHTML = "Patito <br> Feo";
+//innerHTML sirve para modificar el 
+//html desde javascript, el innerText 
+//solo traduce a texto.
+
+h1.innerHTML = "Patito <br> Feo";    
 h1.innerText = "Patito <br> Feo";
 // console.log(h1.getAttribute("class"));
 // h1.setAttribute("class", "rojo");
 
+//classList sirve para modificar la clase de una etiqueta HTML, 
+//agregarle otra, eliminar una, etc.
+
 h1.classList.add("rojo", "azul");
 h1.classList.remove("azul")
+
+//document.createElement lo que hace es crear una etiqueta
+//desde js al HTML.
+
+console.log(document.createElement("img"))
 
 
