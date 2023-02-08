d6664c6a8748a66a4df098bce02dc8bf4920fbdc
Gino <ginococeres2000@gmail.com>
2023-02-08 12:39:19 -0300

    Agregue una segunda y tercer clase, y luego borre una

---
+++ script.js
@@ -0,0 +1,25 @@
+const h1 = document.querySelector("h1");
+const p = document.querySelector("p");
+const parrafito = document.getElementsByClassName(".parrafito");
+const pid = document.getElementById("pid");
+const input = document.querySelector("input");
+
+console.log(input.value);
+
+console.log({
+  h1,
+  p,
+  parrafito,
+  pid,
+  input,
+})
+
+h1.innerHTML = "Patito <br> Feo";
+h1.innerText = "Patito <br> Feo";
+// console.log(h1.getAttribute("class"));
+// h1.setAttribute("class", "rojo");
+
+h1.classList.add("rojo", "azul");
+h1.classList.remove("azul")
+
+
