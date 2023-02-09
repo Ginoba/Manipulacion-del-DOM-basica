b348e419f87abfbc67496d778978ad1f083788d7
Gino <ginococeres2000@gmail.com>
2023-02-09 19:38:24 -0300

    Se agrego una funcion

--- script_dos.js
+++ script_dos.js
@@ -1,4 +1,8 @@
 const h1 = document.querySelector("h1");
-const input1 = document.querySelector("input");
-const input2 = document.querySelector("input");
+const input1 = document.querySelector("calculo1");
+const input2 = document.querySelector("calculo2");
 const btn = document.querySelector()
+
+function btnOnClick () {
+  console.log("Escuchando el evento del click");
+}
