01d9e13b4aae20066c68ebf544e8fa956f83c671
Gino <ginococeres2000@gmail.com>
2023-02-10 05:39:56 -0300

    Se agrego un eventlistener, se cambio el html y se agregaron variables al .js

--- script_dos.js
+++ script_dos.js
@@ -1,8 +1,15 @@
 const h1 = document.querySelector("h1");
-const input1 = document.querySelector("calculo1");
-const input2 = document.querySelector("calculo2");
-const btn = document.querySelector()
+const form = document.querySelector("#form");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const pResult = document.querySelector("#result");
 
-function btnOnClick () {
-  console.log("Escuchando el evento del click");
+form.addEventListener("submit", sumarInputValues);
+
+function sumarInputValues (event) {
+  console.log({event});
+  event.preventDefault();
+  const sumaInputs = console.log(parseInt(input1.value) + parseInt(input2.value));
+  pResult.innerText = "Resultado: " + sumaInputs;
 }
